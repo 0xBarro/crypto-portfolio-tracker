b268cff1950489c7227726e2f38ba66768e9ac57
0xBarro <asfasxfaxfdsfxsfxasdxfsadfx@protonmail.com>
2021-12-31 11:49:25 +0000

    Updated tests

--- __tests__/test_consts.ts
+++ __tests__/test_consts.ts
@@ -1,6 +1,7 @@
-import { polygon } from "../lib/blockchainExplorers/ethScanLike/consts"
+import constObj from "../lib/blockchainExplorers/ethScanLike/consts"
 
 test('Test Polygon Object', () => {
+    const polygon = constObj['polygon']
     expect(polygon.getNormalTxUrl('testW', 'token')).toBe("https://api.polygonscan.com/api?module=account&action=txlist&address=testW&startblock=1&endblock=99999999&sort=asc&apikey=token");
     expect(polygon.getTxUrl('tx')).toBe("https://polygonscan.com/tx/tx");
   });
--- lib/blockchainExplorers/ethScanLike/consts.ts
+++ lib/blockchainExplorers/ethScanLike/consts.ts
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv'
 dotenv.config()
 
-export const polygon = {
+const polygon = {
     getTxEndpointUrl(address: string, action: string, _token?: string): string {
         const token = (_token === undefined) ? process.env['POLYGONSCAN_KEY'] : _token // This is just to allow for the testing
         return `https://api.polygonscan.com/api?module=account&action=${action}&address=${address}&startblock=1&endblock=99999999&sort=asc&apikey=${token}`
@@ -12,4 +12,9 @@ export const polygon = {
     getERC721TxUrl(address: string, _token?: string): string {return this.getTxEndpointUrl(address, 'tokenfttx', _token)},
     getTxUrl(tx: string): string {return `https://polygonscan.com/tx/${tx}`},
     getAddressUrl(address: string): string {return `https://polygonscan.com/address/${address}`}
+}
+
+
+export default {
+    polygon: polygon
 }
