12d77f9abf6c11a0d2f518def280eb434607bcf0
0xBarro <asfasxfaxfdsfxsfxasdxfsadfx@protonmail.com>
2021-12-31 14:22:46 +0000

    Added more functions to utils

--- __tests__/test_utils.ts
+++ __tests__/test_utils.ts
@@ -2,5 +2,7 @@ import {processTimestamp} from '../lib/utils'
 
 test('Assert timestamp transform from Ethscan API', () => {
     const res = processTimestamp(1590858100)
-    expect(res.dateStr).toBe('5/30/2020, 6:01:40 PM')
+    expect(res.dateStr).toBe('Sat, 30 May 2020 17:01:40 GMT')
+    expect(res.dateOnly).toBe('30/5/2020')
+    expect(Object.keys(res).length).toBe(3)
   });
--- lib/utils.ts
+++ lib/utils.ts
@@ -2,10 +2,14 @@ const getJson = async (url: string): Promise<JSON> => {
     return fetch(url).then(r => r.json())
 }
 
-const processTimestamp = (timestamp: number, mult=1000, onlyDate=false): {dateObj: Date, dateStr: string} => {
+const dateToDateStr = (date: Date): string => {
+    return `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`
+}
+
+const processTimestamp = (timestamp: number, mult=1000): {dateObj: Date, dateStr: string, dateOnly: string} => {
     const dateObj =  new Date(timestamp*mult)
-    const dateStr = (onlyDate) ? dateObj.toLocaleDateString() : dateObj.toLocaleString()
-    return {dateObj: dateObj, dateStr: dateStr}
+    const dateStr =  dateObj.toUTCString()  
+    return {dateObj: dateObj, dateStr: dateStr, dateOnly: dateToDateStr(dateObj)}
 }
 
 export {getJson, processTimestamp}
