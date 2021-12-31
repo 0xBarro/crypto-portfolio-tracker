3e27447624755c84c2d4e4b964f882f3a1292cec
0xBarro <asfasxfaxfdsfxsfxasdxfsadfx@protonmail.com>
2021-12-31 20:59:54 +0000

    Added interface to the consts

--- lib/blockchainExplorers/ethScanLike/consts.ts
+++ lib/blockchainExplorers/ethScanLike/consts.ts
@@ -1,7 +1,21 @@
 import dotenv from 'dotenv'
 dotenv.config()
 
-const polygon = {
+interface ethscanSetupObg {
+    getTxEndpointUrl: (address: string, action: string, _token?: string) => string,
+    getInternalTxUrl: (address: string,  _token?: string) => string,
+    getNormalTxUrl: (address: string, _token?: string) => string,
+    getERC20TxUrl: (address: string,  _token?: string) => string,
+    getERC721TxUrl: (address: string, _token?: string) => string,
+    getTxUrl: (tx: string) => string,
+    getAddressUrl: (address: string) => string,
+    gasToken: string,
+    wrappedGasToken: string,
+    gasTokenAddress: string,
+    wrappedGasTokenAddress: string
+}
+
+const polygon: ethscanSetupObg = {
     getTxEndpointUrl(address: string, action: string, _token?: string): string {
         const token = (_token === undefined) ? process.env['POLYGONSCAN_KEY'] : _token // This is just to allow for the testing
         return `https://api.polygonscan.com/api?module=account&action=${action}&address=${address}&startblock=1&endblock=99999999&sort=asc&apikey=${token}`
@@ -11,7 +25,11 @@ const polygon = {
     getERC20TxUrl(address: string,  _token?: string): string {return this.getTxEndpointUrl(address, 'tokentx', _token)},
     getERC721TxUrl(address: string, _token?: string): string {return this.getTxEndpointUrl(address, 'tokennfttx', _token)},
     getTxUrl(tx: string): string {return `https://polygonscan.com/tx/${tx}`},
-    getAddressUrl(address: string): string {return `https://polygonscan.com/address/${address}`}
+    getAddressUrl(address: string): string {return `https://polygonscan.com/address/${address}`},
+    gasToken: 'MATIC',
+    wrappedGasToken: 'WMATIC',
+    gasTokenAddress: '0x0000000000000000000000000000000000001010',
+    wrappedGasTokenAddress: '0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270'
 }
 
 const constObj = {
