43271d1a3c681711a922bff0b534c99f4b44d794
0xBarro <asfasxfaxfdsfxsfxasdxfsadfx@protonmail.com>
2022-01-01 13:11:04 +0000

    Added coingecko fucntion

--- lib/priceFeeds/coinGecko.ts
+++ lib/priceFeeds/coinGecko.ts
@@ -3,7 +3,7 @@ import { coinList, processedCoinList, nameMapType, coinHistory } from "./interfa
 import { processedTx } from "../blockchainExplorers/interfaces"
 
 
-const listCoins = async (): Promise<processedCoinList> => {
+export const listCoins = async (): Promise<processedCoinList> => {
     const url = "https://api.coingecko.com/api/v3/coins/list?include_platform=true"
     const r: Array<coinList> = await fetch(url).then(r => r.json())
 
@@ -11,12 +11,12 @@ const listCoins = async (): Promise<processedCoinList> => {
 }
 
 // Platform name map
-const platformNameMap: nameMapType = {
+export const platformNameMap: nameMapType = {
     "polygon-pos": 'polygon',
     "harmony-shard-0": 'harmony'
 }
 
-const processCoinsList = (coinsList: Array<coinList>): processedCoinList => {
+export const processCoinsList = (coinsList: coinList[]): processedCoinList => {
     const contractDict: processedCoinList = {} // Stores tokens by platform, by contract address. If there is not contract address, then stores id
 
     // Cycle through each listed platform
@@ -45,14 +45,14 @@ const processCoinsList = (coinsList: Array<coinList>): processedCoinList => {
     return contractDict
 }
 
-const getCoinHistory = async (token: string, currency='eur', days=365): Promise<coinHistory> => {
+export const getCoinHistory = async (token: string, currency='eur', days=365): Promise<coinHistory> => {
     const url = `https://api.coingecko.com/api/v3/coins/${token}/market_chart?vs_currency=${currency}&days=${days}`
     const r: Array<[number, number]> = await fetch(url).then(r => r.json()).then(x => x['prices'])
     const processedResult:coinHistory = Object.fromEntries(r.map(p => [processTimestamp(p[0], 1).dateStr, p[1]]))
     return processedResult
 }
 
-const getTokenPriceAtDate = (tokenHistory: coinHistory, dateStr: string): number | undefined => {
+export const getTokenPriceAtDate = (tokenHistory: coinHistory, dateStr: string): number | undefined => {
     if (dateStr in tokenHistory){tokenHistory[dateStr]}
     else {return undefined}
 }
