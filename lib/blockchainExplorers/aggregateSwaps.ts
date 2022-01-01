b7323cbde46db0e27b8aefba0b29d4d99734ad28
0xBarro <asfasxfaxfdsfxsfxasdxfsadfx@protonmail.com>
2022-01-01 20:54:38 +0000

    Changed aggregate swaps location

---
+++ lib/blockchainExplorers/aggregateSwaps.ts
@@ -0,0 +1,8 @@
+import { processedSwap, processedTx } from "./interfaces"
+
+// Aggregates all the swaps for each wallet, and calculated capital gains
+const aggregateSwaps = (txList: processedTx[], wallet: string, chain: string) => {
+    1
+}
+
+export default aggregateSwaps
