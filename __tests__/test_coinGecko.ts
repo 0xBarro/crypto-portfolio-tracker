cec9caea9c18477687720ec4699ed68c2cb40fd7
0xBarro <asfasxfaxfdsfxsfxasdxfsadfx@protonmail.com>
2022-01-01 13:10:47 +0000

    Implemented tests for Coingecko function

--- __tests__/test_coinGecko.ts
+++ __tests__/test_coinGecko.ts
@@ -1,10 +1,24 @@
 import fs from 'fs'
-import {coinList} from '../lib/priceFeeds/interfaces'
-import {processCoinsList} from '../lib/priceFeeds/coinGecko'
+import { coinList } from '../lib/priceFeeds/interfaces'
+import { processCoinsList } from '../lib/priceFeeds/coinGecko'
 
 test('Test processCoinsList', async () => {
     const testFilePath = '__tests__/testFiles/coinGecko/processCoinsList.json'
-    const file = JSON.parse((fs.readFileSync(testFilePath).toString()))
+    const file: coinList[] = JSON.parse((fs.readFileSync(testFilePath).toString()))
     const processedFile = processCoinsList(file)
-    console.log(processedFile)
+
+    expect(processedFile['platform1']).toStrictEqual({
+        '0x123': {
+            name: 'TestCoin1Name',
+            id: 'TestCoin1',
+            symbol: 'TestCoint1Symbol',
+            platforms: { platform1: '0x123', platform2: '0x1234'}
+        },
+        '0x1234': {
+            name: 'TestCoin2Name',
+            id: 'TestCoin2',
+            symbol: 'TestCoint2Symbol',
+            platforms: { platform1: '0x1234'}
+        }
+    })
 })
