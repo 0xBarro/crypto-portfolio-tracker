30df3b484d538911569bb5e72f75afbe4ebc8de9
0xBarro <asfasxfaxfdsfxsfxasdxfsadfx@protonmail.com>
2022-01-05 19:00:44 +0000

    Added more tests

--- __tests__/test_buyQ.ts
+++ __tests__/test_buyQ.ts
@@ -1,13 +1,22 @@
 import buyQueue from "../lib/blockchainExplorers/buyQueue"
+import { buyQueueReg } from "../lib/blockchainExplorers/interfaces"
+
+const totalTokenAmount = (bq: buyQueueReg[]): number => {
+    return bq.map(e => e.amount).reduce((l, r) => l + r)
+}
 
 test('Test the buyQ results', async () => {
     const buyQ = new buyQueue()
 
     const tokenCA = 'buyTokenCA'
     buyQ.addBuy(tokenCA, '01/01/2000', 100, 1)
+    buyQ.addBuy(tokenCA, '02/01/2000', 100, 1)
+    buyQ.addBuy(tokenCA, '03/01/2000', 100, 1)
     expect(tokenCA in buyQ.buyHist).toBeTruthy()
+    expect(totalTokenAmount(buyQ.buyHist[tokenCA])).toBe(300)
 
     const res = buyQ.calcGainsSell(tokenCA, '01/02/2000', 50, 2)
-    expect(res.capGains).toBe(100)
+    expect(totalTokenAmount(buyQ.buyHist[tokenCA])).toBe(250)
+    expect(res.capGains).toBe(50)
 
 })
